c232e161fd800cae182f7a982647153300dde5b2
Artem Pronin <artemalexp@gmail.com>
2023-09-02 23:34:14 +0300

    Updates

---
+++ static/js/NotFound.b9fe3a62.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpizza=self.webpackChunkpizza||[]).push([[187],{4949:function(u,e,n){n.r(e);var a=n(6749),s=n(184);e.default=function(){return(0,s.jsx)(a.Z,{})}}}]);
+//# sourceMappingURL=NotFound.b9fe3a62.chunk.js.map
